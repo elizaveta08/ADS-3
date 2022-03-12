8c50b6477d6e513bbdc5a6b2577bfbbc345db44f
Sakhno Elizaveta <97175260+elizaveta08@users.noreply.github.com>
2022-03-12 22:52:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,16 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+ int summa, i;
+ summa = 0;
+  for (i = 0; size > i; i++) {
+    if (arr[i] == value) {
+      summa++;
+    }
+  }
+  if (summa != 0) {
+    return summa;
+  } else {
+    return 0; // если ничего не найдено
+  }
 }
